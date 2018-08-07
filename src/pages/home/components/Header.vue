ff185922f3e32e64754b57c89852ad1bae9d03ae
wuxinning <you@email.address>
2018-08-07 08:32:39 +0800

    project

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <header id="top">
+    <div class="topList">
+      <!--切换地址-->
+      <div class="address">
+        <span>北京</span>
+        <div class="top_show addressList">
+          <ul>
+            <li class="province"><a href="javascript:" class="add_checked">北京</a></li>
+            <li class="province"><a href="javascript:">上海</a></li>
+            <li class="province"><a href="javascript:">天津</a></li>
+            <li class="province"><a href="javascript:">重庆</a></li>
+            <li class="province"><a href="javascript:">河北</a></li>
+            <li class="province"><a href="javascript:">山西</a></li>
+            <li class="province"><a href="javascript:">河南</a></li>
+            <li class="province"><a href="javascript:">辽宁</a></li>
+            <li class="province"><a href="javascript:">吉林</a></li>
+            <li class="province"><a href="javascript:">黑龙江</a></li>
+            <li class="province"><a href="javascript:">内蒙古</a></li>
+            <li class="province"><a href="javascript:">江苏</a></li>
+            <li class="province"><a href="javascript:">山东</a></li>
+            <li class="province"><a href="javascript:">安徽</a></li>
+            <li class="province"><a href="javascript:">浙江</a></li>
+            <li class="province"><a href="javascript:">福建</a></li>
+            <li class="province"><a href="javascript:">湖北</a></li>
+            <li class="province"><a href="javascript:">湖南</a></li>
+            <li class="province"><a href="javascript:">广东</a></li>
+            <li class="province"><a href="javascript:">广西</a></li>
+            <li class="province"><a href="javascript:">江西</a></li>
+            <li class="province"><a href="javascript:">四川</a></li>
+            <li class="province"><a href="javascript:">海南</a></li>
+            <li class="province"><a href="javascript:">贵州</a></li>
+            <li class="province"><a href="javascript:">云南</a></li>
+            <li class="province"><a href="javascript:">西藏</a></li>
+            <li class="province"><a href="javascript:">陕西</a></li>
+            <li class="province"><a href="javascript:">甘肃</a></li>
+            <li class="province"><a href="javascript:">青海</a></li>
+            <li class="province"><a href="javascript:">宁夏</a></li>
+            <li class="province"><a href="javascript:">新疆</a></li>
+            <li class="province"><a href="javascript:">港澳</a></li>
+            <li class="province"><a href="javascript:">台湾</a></li>
+            <li class="province"><a href="javascript:">钓鱼岛</a></li>
+            <li class="province"><a href="javascript:">海外</a></li></ul>
+        </div>
+      </div>
+      <!--切换地址结束-->
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "../../../assets/styles/header.styl"
+  /*顶部定位区域*/
+  /* 顶部定位区域滑过样式 */
+  /* 顶部选择定位地域列表 */
+</style>
